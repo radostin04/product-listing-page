d62011bce205962d205891fb5262b60ae51c3c09
radostin04 <mitiku04@gmail.com>
2023-07-26 21:58:17 +0300

    fix style issues with star rating

--- src/components/productTile/starsRating.tsx
+++ src/components/productTile/starsRating.tsx
@@ -7,13 +7,13 @@ const StarsRating: React.FC<{stars: number}> = ({stars}) => {
   let starsArray:JSX.Element[] = [];
   for(let i = 1; i <= 5; i++) {
     if(i <= starsToFill) {
-      starsArray.push(<span key={i} className={classes.full}>&#xf000;</span>)
+      starsArray.push(<div key={i} className={classes.full}></div>)
     } else {
       if(shouldFillHalfStar) {
-        starsArray.push(<span key={i} className={classes.half}>&#xf000;</span>)
+        starsArray.push(<div key={i} className={classes.half}></div>)
         shouldFillHalfStar = false
       } else {
-        starsArray.push(<span key={i} className={classes.empty}>&#xf000;</span>)
+        starsArray.push(<div key={i} className={classes.empty}></div>)
       }
     }
   }
