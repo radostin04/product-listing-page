4fcabe78496ef02b9953f9ddc5574c72e331d6c8
radostin04 <mitiku04@gmail.com>
2023-07-26 21:58:09 +0300

    show final price in product tiles + styling

--- src/components/productTile/productTile.tsx
+++ src/components/productTile/productTile.tsx
@@ -6,22 +6,29 @@ const ProductTile: React.FC<{
   name: string;
   description: string;
   price: number;
-  discount?: number;
+  discount: number | null;
   rating: number;
   image: string;
   onClickButton: MouseEventHandler<HTMLButtonElement>;
 }> = (props) => {
   const priceClasses = props.discount ? `${classes.price} ${classes.discount}` : `${classes.price}`;
+
+  let finalPrice = props.price.toString();
+  if(props.discount) {
+    const discountAmount = props.price * (props.discount / 100);
+    finalPrice = (Math.round((props.price - discountAmount) * 100) / 100).toFixed(2);
+  }
+
   return (
     <div className={classes.container}>
       <img src={props.image} alt=""></img>
       <h6>{props.name}</h6>
-      <p>{props.description}</p>
       <StarsRating stars={props.rating}></StarsRating>
+      <p>{props.description}</p>
       <div className={classes.priceAndAddContainer}>
         <div className={priceClasses}>
-          <p>${props.price}</p>
-          {props.discount ? <p>-{props.discount}%</p> : null}
+          <p className={classes.priceText}>${finalPrice}</p>
+          {props.discount ? <p className={classes.discountText}>-{props.discount}%</p> : null}
         </div>
         <div className={classes.addButton}>
           <button onClick={props.onClickButton}>Add to Cart</button>
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export type Product = {
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  color: string;
+  rating: number;
+  discount: number | null
+}
